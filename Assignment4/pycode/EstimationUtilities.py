2ebc0d22eba3aa54a0518fa7917db2b6373dadbf
FranzPaj <francescopajero@gmail.com>
2024-04-16 23:34:48 +0200

    Work on Q4
    Processing of new measurements

--- Assignment4/pycode/EstimationUtilities.py
+++ Assignment4/pycode/EstimationUtilities.py
@@ -3,6 +3,7 @@ import math
 from datetime import datetime, timedelta
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 import pickle
 
 if __name__ == '__main__':
@@ -118,7 +119,6 @@ def read_measurement_file(meas_file):
     pklFile.close()
     
     return state_params, meas_dict, sensor_params
-
 
 
 ###############################################################################
@@ -523,7 +523,6 @@ def compute_measurement(tk, X, sensor_params):
     return Y
 
 
-
 def define_radar_sensor(latitude_rad, longitude_rad, height_m):
     '''
     This function will generate the sensor parameters dictionary for a radar
@@ -686,6 +685,218 @@ def define_optical_sensor(latitude_rad, longitude_rad, height_m):
     return sensor_params
 
 
+###############################################################################
+# Measurement processing
+###############################################################################
+
+def optical_analysis(truth_state, state, meas,
+             sensor, integ, filters, time, name: str, mtype, plotting):
+    rad2arcsec = 1 / ((1. / 3600.) * np.pi / 180.)
+    filter_output = ukf(state, meas, sensor,
+                             integ, filters, None)
+    # extract covars and state
+    covars = np.vstack(
+        [(filter_output[t])['covar'].reshape(1, 36) for t in time])
+    state = np.vstack(
+        [(filter_output[t])['state'].reshape(1, 6) for t in time])
+    dummy = len((filter_output[time[0]])['resids'])
+    resid = np.vstack(
+        [(filter_output[t])['resids'].reshape(1, dummy) for t in time])
+
+    error_ric = np.zeros((len(time), 3))
+
+    error = truth_state - state
+
+    cov_pos_ric = np.zeros((len(time), 9))
+
+    for i in range(len(time)):
+        # int state
+        error_ric[i, :] = eci2ric(
+            state[i, 0:3], state[i, 3:6], error[i, :3]).reshape(1, 3)
+        cov = covars[i, :].reshape(6, 6)
+        cov_pos_ric[i, :] = (eci2ric(
+            state[i, 0:3], state[i, 3:6], cov[:3, :3])).reshape(1, 9)
+
+    # sigma bound
+    sigma_bound_pos = 3 * np.sqrt(cov_pos_ric)
+
+    RMS_pos = np.sqrt(np.sum(error[:, 0:3]**2)/len(time))
+    RMS_vel = np.sqrt(np.sum(error[:, 3:6]**2)/len(time))
+    if mtype == 'opt':
+        RMS_RA = np.sqrt(np.sum((resid[:, 0]*rad2arcsec) ** 2) / len(time))
+        RMS_DEC = np.sqrt(np.sum((resid[:, 1]*rad2arcsec) ** 2) / len(time))
+        print(RMS_RA)
+        print(RMS_DEC)
+    if mtype == 'rad':
+        RMS_range = np.sqrt(np.sum(resid[:, 0] ** 2) / len(time))
+        RMS_RA = np.sqrt(np.sum((resid[:, 1]*rad2arcsec) ** 2) / len(time))
+        RMS_DEC = np.sqrt(np.sum((resid[:, 2]*rad2arcsec) ** 2) / len(time))
+        print(RMS_range)
+        print(RMS_RA)
+        print(RMS_DEC)
+
+    if plotting:
+        plt.rc('axes', titlesize=16)  # fontsize of the axes title
+        plt.rc('axes', labelsize=16)  # fontsize of the x and y labels
+        plt.rc('xtick', labelsize=14)  # fontsize of the tick labels
+        plt.rc('ytick', labelsize=14)  # fontsize of the tick labels
+        plt.rc('legend', fontsize=14)  # legend fontsize
+        # plotting 1
+        time_rel = (time - time[0]) / 3600
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+        ax1.plot(time_rel, error_ric[:, 0])
+        ax1.plot(time_rel, sigma_bound_pos[:, 0], 'k:')
+        ax1.plot(time_rel, -sigma_bound_pos[:, 0], 'k:')
+        ax1.grid()
+        ax2.plot(time_rel, error_ric[:, 1])
+        ax2.plot(time_rel, sigma_bound_pos[:, 4], 'k:')
+        ax2.plot(time_rel, -sigma_bound_pos[:, 4], 'k:')
+        ax2.grid()
+        ax3.plot(time_rel, error_ric[:, 2])
+        ax3.plot(time_rel, sigma_bound_pos[:, 8], 'k:')
+        ax3.plot(time_rel, -sigma_bound_pos[:, 8], 'k:')
+        ax3.grid()
+        plt.tight_layout()
+        plt.savefig('plots/' + name + '_error.png')
+
+        fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8),
+              (ax9, ax10, ax11, ax12)) = plt.subplots(
+            3, 4, figsize=(12, 8))
+        # R direction
+        ax1.plot(time_rel[:31], error_ric[:31, 0])
+        ax1.plot(time_rel[:31], sigma_bound_pos[:31, 0], 'k:')
+        ax1.plot(time_rel[:31], -sigma_bound_pos[:31, 0], 'k:')
+        ax1.grid()
+        ax1.set_ylabel('R-direction [m]', size=18)
+        ax2.plot(time_rel[31:62], error_ric[31:62, 0])
+        ax2.plot(time_rel[31:62], sigma_bound_pos[31:62, 0], 'k:')
+        ax2.plot(time_rel[31:62], -sigma_bound_pos[31:62, 0], 'k:')
+        ax2.grid()
+        ax3.plot(time_rel[62:93], error_ric[62:93, 0])
+        ax3.plot(time_rel[62:93], sigma_bound_pos[62:93, 0], 'k:')
+        ax3.plot(time_rel[62:93], -sigma_bound_pos[62:93, 0], 'k:')
+        ax3.grid()
+        ax4.plot(time_rel[93:], error_ric[93:, 0])
+        ax4.plot(time_rel[93:], sigma_bound_pos[93:, 0], 'k:')
+        ax4.plot(time_rel[93:], -sigma_bound_pos[93:, 0], 'k:')
+        ax4.grid()
+        # I direction
+        ax5.plot(time_rel[:31], error_ric[:31, 1])
+        ax5.set_ylabel('I-direction [m]', size=18)
+        ax5.plot(time_rel[:31], sigma_bound_pos[:31, 4], 'k:')
+        ax5.plot(time_rel[:31], -sigma_bound_pos[:31, 4], 'k:')
+        ax5.grid()
+        ax6.plot(time_rel[31:62], error_ric[31:62, 1])
+        ax6.plot(time_rel[31:62], sigma_bound_pos[31:62, 4], 'k:')
+        ax6.plot(time_rel[31:62], -sigma_bound_pos[31:62, 4], 'k:')
+        ax6.grid()
+        ax7.plot(time_rel[62:93], error_ric[62:93, 1])
+        ax7.plot(time_rel[62:93], sigma_bound_pos[62:93, 4], 'k:')
+        ax7.plot(time_rel[62:93], -sigma_bound_pos[62:93, 4], 'k:')
+        ax7.grid()
+        ax8.plot(time_rel[93:], error_ric[93:, 1])
+        ax8.plot(time_rel[93:], sigma_bound_pos[93:, 4], 'k:')
+        ax8.plot(time_rel[93:], -sigma_bound_pos[93:, 4], 'k:')
+        ax8.grid()
+        # C direction
+        ax9.plot(time_rel[:31], error_ric[:31, 2])
+        ax9.plot(time_rel[:31], sigma_bound_pos[:31, 8], 'k:')
+        ax9.plot(time_rel[:31], -sigma_bound_pos[:31, 8], 'k:')
+        ax9.set_ylabel('C-direction [m]', size=18)
+        ax9.set_xlabel('Time [hours]', size=18)
+        ax9.grid()
+        ax10.plot(time_rel[31:62], error_ric[31:62, 2])
+        ax10.plot(time_rel[31:62], sigma_bound_pos[31:62, 8], 'k:')
+        ax10.plot(time_rel[31:62], -sigma_bound_pos[31:62, 8], 'k:')
+        ax10.set_xlabel('Time [hours]', size=18)
+        ax10.grid()
+        ax11.plot(time_rel[62:93], error_ric[62:93, 2])
+        ax11.plot(time_rel[62:93], sigma_bound_pos[62:93, 8], 'k:')
+        ax11.plot(time_rel[62:93], -sigma_bound_pos[62:93, 8], 'k:')
+        ax11.set_xlabel('Time [hours]', size=18)
+        ax11.grid()
+        ax12.plot(time_rel[93:], error_ric[93:, 2])
+        ax12.plot(time_rel[93:], sigma_bound_pos[93:, 8], 'k:')
+        ax12.plot(time_rel[93:], -sigma_bound_pos[93:, 8], 'k:')
+        ax12.set_xlabel('Time [hours]', size=18)
+        ax12.grid()
+        plt.tight_layout()
+        plt.savefig('plots/' + name + '_error_sparse.png')
+
+        if mtype == 'rad':
+
+            fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8),
+                (ax9, ax10, ax11, ax12)) = plt.subplots(
+                3, 4, figsize=(12, 8))
+            # R direction
+            ax1.scatter(time_rel[:31], resid[:31, 0])
+            ax1.grid()
+            ax1.set_ylabel('Range [m]', size=18)
+            ax2.scatter(time_rel[31:62], resid[31:62, 0])
+            ax2.grid()
+            ax3.scatter(time_rel[62:93], resid[62:93, 0])
+            ax3.grid()
+            ax4.scatter(time_rel[93:], resid[93:, 0])
+            ax4.grid()
+            # I direction
+            ax5.scatter(time_rel[:31], resid[:31, 1]*rad2arcsec)
+            ax5.set_ylabel('RA [arcsec]', size=18)
+            ax5.grid()
+            ax6.scatter(time_rel[31:62], resid[31:62, 1]*rad2arcsec)
+            ax6.grid()
+            ax7.scatter(time_rel[62:93], resid[62:93, 1]*rad2arcsec)
+            ax7.grid()
+            ax8.scatter(time_rel[93:], resid[93:, 1]*rad2arcsec)
+            ax8.grid()
+            # C direction
+            ax9.scatter(time_rel[:31], resid[:31, 2]*rad2arcsec)
+            ax9.set_ylabel('DEC [arcsec]', size=18)
+            ax9.set_xlabel('Time [hours]', size=18)
+            ax9.grid()
+            ax10.scatter(time_rel[31:62], resid[31:62, 2]*rad2arcsec)
+            ax10.set_xlabel('Time [hours]', size=18)
+            ax10.grid()
+            ax11.scatter(time_rel[62:93], resid[62:93, 2]*rad2arcsec)
+            ax11.set_xlabel('Time [hours]', size=18)
+            ax11.grid()
+            ax12.scatter(time_rel[93:], resid[93:, 2]*rad2arcsec)
+            ax12.set_xlabel('Time [hours]', size=18)
+            ax12.grid()
+            plt.tight_layout()
+            plt.savefig('plots/' + name + '_residuals.png')
+
+        elif mtype == 'opt':
+            rad2arcsec = 3600. * 180. / np.pi
+            fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8)) = plt.subplots(
+                2, 4, figsize=(16, 10))
+            # R direction
+            ax1.scatter(time_rel[:31], resid[:31, 0] * rad2arcsec)
+            ax1.set_ylabel('RA [arcsec]', size=18)
+            ax1.grid()
+            ax2.scatter(time_rel[31:62], resid[31:62, 0] * rad2arcsec)
+            ax2.grid()
+            ax3.scatter(time_rel[62:93], resid[62:93, 0] * rad2arcsec)
+            ax3.grid()
+            ax4.scatter(time_rel[93:], resid[93:, 0] * rad2arcsec)
+            ax4.grid()
+            # I direction
+            ax5.scatter(time_rel[:31], resid[:31, 1] * rad2arcsec)
+            ax5.set_ylabel('DEC [arcsec]', size=18)
+            ax5.set_xlabel('Time [hours]', size=18)
+            ax5.grid()
+            ax6.scatter(time_rel[31:62], resid[31:62, 1] * rad2arcsec)
+            ax6.set_xlabel('Time [hours]', size=18)
+            ax6.grid()
+            ax7.scatter(time_rel[62:93], resid[62:93, 1] * rad2arcsec)
+            ax7.set_xlabel('Time [hours]', size=18)
+            ax7.grid()
+            ax8.scatter(time_rel[93:], resid[93:, 1] * rad2arcsec)
+            ax8.set_xlabel('Time [hours]', size=18)
+            ax8.grid()
+            plt.tight_layout()
+            plt.savefig('plots/' + name + '_residuals.png')
+
+    return RMS_pos, RMS_vel
 
 
 
